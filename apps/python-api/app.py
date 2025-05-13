8e43a305c5017b6d8af57764d4c507fc349541b3
Hendrew Gustavo <113458822+Hendr3w@users.noreply.github.com>
2025-05-13 20:55:43 -0300

    API Flask e banco funcionando.

---
+++ apps/python-api/app.py
@@ -0,0 +1,32 @@
+from flask import Flask 
+from dotenv import load_dotenv
+import mysql.connector
+import os
+
+load_dotenv()
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def hello():
+    db = mysql.connector.connect(
+        user="root",          
+        host="dbmysql",      
+        password="root",     
+        database="flaskdb",  
+        port="3306",         
+        auth_plugin='mysql_native_password'
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT 'Hello from flask MYSQL'")
+    result = cursor.fetchone()
+    cursor.close()
+    db.close()
+    return result[0]
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
